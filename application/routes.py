e851b84fed12ef7ed867abc69f0f72f9c7f424a8
Kholeo Taylor (Virtual Machine) <kholeotaylor@hotmail.co.uk>
2020-11-13 10:50:32 +0000

    added delete button

--- application/routes.py
+++ application/routes.py
@@ -21,6 +21,21 @@ def add():
         return redirect(url_for('index'))
     return render_template('add.html', form=form)
 
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form  = ListingForm()
+    listing = Listing.query.get(id)
+    if form.validate_on_submit():
+        listing_title = form.title.data,
+        listing_description = form.list_description.data
+        db.session.commit()
+        return redirect(url_for('index'))
+    elif request.method == 'GET':
+        form.title.data = listing_title,
+        form.list_description.data = listing_description
+    return render_template('update.html', title='Update Listing', form=form)
+
+
 @app.route('/delete/<int:id>')
 def delete(id):
     delete_listing = Listing.query.get(id)
